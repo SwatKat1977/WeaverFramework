ae4a7b6989c9d45df61ca3ce3793a7f6d4276c65
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-05-16 23:04:48 +0100

    Cleanup

--- tests/main.py
+++ tests/main.py
@@ -1,8 +1,8 @@
 import unittest
-from tests.common.test_configuration_configuration_manager import TestConfigurationManager
-from tests.common.test_configuration_setup import (TestConfigItemDataType,
-                                                   TestConfigurationSetupItem,
-                                                   TestConfigurationSetup)
+from tests.test_configuration_configuration_manager import TestConfigurationManager
+from tests.test_configuration_setup import (TestConfigItemDataType,
+                                            TestConfigurationSetupItem,
+                                            TestConfigurationSetup)
 
 
 if __name__ == "__main__":  # pragma: no cover
