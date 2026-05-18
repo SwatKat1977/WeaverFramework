815adeeb7491f211a1e167f4472bd3c1cd16e7cd
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-05-18 20:53:21 +0100

    Split decorator into own module as where it was currently was incorrect

--- tests/main.py
+++ tests/main.py
@@ -3,8 +3,9 @@ from tests.test_configuration_configuration_manager import TestConfigurationMana
 from tests.test_configuration_setup import (TestConfigItemDataType,
                                             TestConfigurationSetupItem,
                                             TestConfigurationSetup)
-from tests.test_microservice_base_api_route import TestMicroserviceBaseApiRoute
 from tests.test_microservice_api_response import TestMicroserviceApiResponse
+from tests.test_microservice_base_api_route import TestMicroserviceBaseApiRoute
+from tests.test_microservice_http_content_type import TestMicroserviceHttpContentType
 from tests.test_version import TestVersion
 
 
